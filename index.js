e8b93c16ad413a8c92f5c93b6db18e1f4f37e6fb
R A <rebecka.arlestedt@gmail.com>
2019-10-16 16:30:09 +0200

    Ny commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function createCircle(radius) {
         radius,// variable, will set value outside function
             draw: function(){
                 console.log('draw');
-            }
+           }
     };
 }
 
@@ -13,7 +13,7 @@ const circle = createCircle(1);
 //console.log(circle);
 //console.log('test');
 
-//Constructor function
+//Constructor function does not need a return
 function Circle(radius){
     this.radius = radius;
     this.draw = function(){
@@ -21,4 +21,4 @@ function Circle(radius){
     }
 }
 const another = new Circle(1); //If i dont use "new" the variable becomes global = bad 
-const hej = hej; 
+console.log(another);
